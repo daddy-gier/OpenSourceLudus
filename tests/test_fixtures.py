ffad13ef35e8d7b828dea8e6712d40fe1a62243d
daddy-gier <gierlegacy@icloud.com>
2026-01-05 04:35:25 -0600

    Add test suite scaffolding

---
+++ tests/test_fixtures.py
@@ -0,0 +1,14 @@
+"""Sample data fixtures for tests."""
+
+SAMPLE_REACT_COMPONENT = """
+import React from 'react';
+
+export default function App() {
+    return <div>Hello, world</div>;
+}
+"""
+
+SAMPLE_PYTHON_MODULE = """
+def add(a, b):
+    return a + b
+"""
